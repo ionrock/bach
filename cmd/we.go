38f48f55eedd07fb7b2f3e8cc88d2580d80afa6f
Eric Larson <eric@ionrock.org>
2016-05-26 09:58:15 -0500

    Add logging, glide, and recursive withenv

--- cmd/we.go
+++ cmd/we.go
@@ -3,17 +3,26 @@ package main
 import (
 	"os"
 
+	log "github.com/Sirupsen/logrus"
 	"github.com/codegangsta/cli"
 	"github.com/ionrock/bach"
 )
 
-func We(c *cli.Context) error {
-	return bach.WithEnv(c.String("env"))
+func WeBefore(c *cli.Context) error {
+	bach.InitLogging(c.Bool("debug"))
+
+	if c.String("env") != "" {
+		err := bach.WithEnv(c.String("env"))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	return nil
 }
 
 func main() {
 	app := cli.NewApp()
-	app.Before = We
+	app.Before = WeBefore
 	app.Action = bach.CommandAction
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
@@ -35,6 +44,10 @@ func main() {
 			Name:  "envvar, E",
 			Value: "",
 			Usage: "Override a single environment variable.",
+		},
+		cli.BoolFlag{
+			Name:  "debug, D",
+			Usage: "Turn on debugging output",
 		},
 	}
 
--- withenv.go
+++ withenv.go
@@ -4,41 +4,82 @@ import (
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"os"
+	"strings"
+
+	log "github.com/Sirupsen/logrus"
 )
 
-type Env struct {
-	Vars []EnvVar
-}
-
-type EnvVar struct {
-	Key   string
-	Value string
-}
-
-func NewEnv(path string) (*Env, error) {
+func LoadYaml(path string) ([]map[interface{}]interface{}, error) {
 	b, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
 
-	var container Env
+	// try loading into a list of maps first
+	m := make([]map[interface{}]interface{}, 5)
 
-	err = yaml.Unmarshal(b, &container)
+	err = yaml.Unmarshal(b, &m)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	return m, nil
+}
+
+func flatKey(prefix []string, key string) string {
+	return strings.Join(append(prefix, key), "_")
+}
+
+func FlattenMap(env map[string]string, ev map[interface{}]interface{}, prefix []string) map[string]string {
+	for k, v := range ev {
+		switch t := v.(type) {
+		case string:
+			key := flatKey(prefix, k.(string))
+			env[key] = v.(string)
+		case map[interface{}]interface{}:
+			FlattenMap(env, v.(map[interface{}]interface{}), append(prefix, k.(string)))
+		default:
+			log.Debug("Prefix: ", prefix)
+			log.Debugf("Default: %#v", t)
+		}
+	}
+
+	return env
+}
+
+func FlattenEnv(env []map[interface{}]interface{}) map[string]string {
+	fenv := make(map[string]string)
+	for _, ev := range env {
+		// must be a map
+		FlattenMap(fenv, ev, []string{})
+	}
+	return fenv
+}
+func YamlToEnv(path string) (map[string]string, error) {
+	env, err := LoadYaml(path)
 	if err != nil {
 		return nil, err
 	}
 
-	return &container, nil
+	e := FlattenEnv(env)
+	log.Debug("Flattened Env")
+	for k, v := range e {
+		log.Debugf("%s = %s", k, v)
+	}
+
+	return e, nil
 }
 
 func WithEnv(envfile string) error {
-	env, err := NewEnv(envfile)
+	env, err := YamlToEnv(envfile)
 	if err != nil {
 		panic(err)
 	}
 
-	for _, ev := range env.Vars {
-		err = os.Setenv(ev.Key, ev.Value)
+	for k, v := range env {
+		log.Debugf("export %s=%s", k, v)
+		err = os.Setenv(k, v)
 		if err != nil {
 			panic(err)
 		}
