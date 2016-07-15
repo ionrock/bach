530c63389d9a7fafef0666d56a2410d790410cfb
Eric Larson <eric@ionrock.org>
2016-07-15 15:58:28 -0500

    Support envs and scripts
    The script support is new. Essentially you can run a script that will
    output YAML. I might also try parsing with JSON too.

--- cmd/we.go
+++ cmd/we.go
@@ -10,41 +10,43 @@ import (
 
 func WeBefore(c *cli.Context) error {
 	bach.InitLogging(c.Bool("debug"))
-
-	if c.String("env") != "" {
-		err := bach.WithEnv(c.String("env"))
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-	return nil
+	log.Info("args: ", os.Args[1:])
+	return bach.WithEnv(os.Args[1:])
 }
 
 func main() {
 	app := cli.NewApp()
+	app.Name = "we"
+	app.Usage = "Add environment variables via YAML or scripts before running a command."
 	app.Before = WeBefore
 	app.Action = bach.CommandAction
 	app.Flags = []cli.Flag{
-		cli.StringFlag{
+		cli.StringSliceFlag{
 			Name:  "env, e",
-			Value: "",
 			Usage: "A YAML file to include in the environment.",
 		},
-		cli.StringFlag{
-			Name:  "directory, d",
-			Value: "",
-			Usage: "A directory containing YAML files to recursively applyt to the environment.",
-		},
-		cli.StringFlag{
-			Name:  "alias, a",
-			Value: "",
-			Usage: "A YAML file containing a list of file/directory entries to apply to the environment.",
-		},
-		cli.StringFlag{
+
+		cli.StringSliceFlag{
 			Name:  "envvar, E",
-			Value: "",
 			Usage: "Override a single environment variable.",
 		},
+
+		cli.StringSliceFlag{
+			Name:  "script, s",
+			Usage: "Execute a script that outputs YAML.",
+		},
+
+		// cli.StringFlag{
+		// 	Name:  "directory, d",
+		// 	Value: "",
+		// 	Usage: "A directory containing YAML files to recursively applyt to the environment.",
+		// },
+		// cli.StringFlag{
+		// 	Name:  "alias, a",
+		// 	Value: "",
+		// 	Usage: "A YAML file containing a list of file/directory entries to apply to the environment.",
+		// },
+
 		cli.BoolFlag{
 			Name:  "debug, D",
 			Usage: "Turn on debugging output",
--- withenv.go
+++ withenv.go
@@ -4,12 +4,14 @@ import (
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"strings"
 
 	log "github.com/Sirupsen/logrus"
+	"github.com/flynn/go-shlex"
 )
 
-func LoadYaml(path string) ([]map[interface{}]interface{}, error) {
+func loadYaml(path string) ([]map[interface{}]interface{}, error) {
 	b, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, err
@@ -31,14 +33,14 @@ func flatKey(prefix []string, key string) string {
 	return strings.Join(append(prefix, key), "_")
 }
 
-func FlattenMap(env map[string]string, ev map[interface{}]interface{}, prefix []string) map[string]string {
+func flattenMap(env map[string]string, ev map[interface{}]interface{}, prefix []string) map[string]string {
 	for k, v := range ev {
 		switch t := v.(type) {
 		case string:
 			key := flatKey(prefix, k.(string))
 			env[key] = v.(string)
 		case map[interface{}]interface{}:
-			FlattenMap(env, v.(map[interface{}]interface{}), append(prefix, k.(string)))
+			flattenMap(env, v.(map[interface{}]interface{}), append(prefix, k.(string)))
 		default:
 			log.Debug("Prefix: ", prefix)
 			log.Debugf("Default: %#v", t)
@@ -48,21 +50,22 @@ func FlattenMap(env map[string]string, ev map[interface{}]interface{}, prefix []
 	return env
 }
 
-func FlattenEnv(env []map[interface{}]interface{}) map[string]string {
+func flattenEnv(env []map[interface{}]interface{}) map[string]string {
 	fenv := make(map[string]string)
 	for _, ev := range env {
 		// must be a map
-		FlattenMap(fenv, ev, []string{})
+		flattenMap(fenv, ev, []string{})
 	}
 	return fenv
 }
-func YamlToEnv(path string) (map[string]string, error) {
-	env, err := LoadYaml(path)
+
+func yamlToEnv(path string) (map[string]string, error) {
+	env, err := loadYaml(path)
 	if err != nil {
 		return nil, err
 	}
 
-	e := FlattenEnv(env)
+	e := flattenEnv(env)
 	log.Debug("Flattened Env")
 	for k, v := range e {
 		log.Debugf("%s = %s", k, v)
@@ -71,8 +74,16 @@ func YamlToEnv(path string) (map[string]string, error) {
 	return e, nil
 }
 
-func WithEnv(envfile string) error {
-	env, err := YamlToEnv(envfile)
+type Action interface {
+	Apply() map[string]string
+}
+
+type EnvFile struct {
+	path string
+}
+
+func (e EnvFile) Apply() map[string]string {
+	env, err := yamlToEnv(e.path)
 	if err != nil {
 		panic(err)
 	}
@@ -84,5 +95,64 @@ func WithEnv(envfile string) error {
 			panic(err)
 		}
 	}
+
+	return env
+}
+
+type EnvScript struct {
+	cmd string
+}
+
+func (e EnvScript) Apply() map[string]string {
+	tmp, err := ioutil.TempFile("", "")
+	if err != nil {
+		panic(err)
+	}
+
+	parts, err := shlex.Split(e.cmd)
+	if err != nil {
+		panic(err)
+	}
+
+	cmd := exec.Command(parts[0], parts[1:]...)
+	cmd.Stdout = tmp
+	err = cmd.Run()
+
+	if err != nil {
+		panic(err)
+	}
+
+	tmp.Close()
+
+	ef := EnvFile{path: tmp.Name()}
+
+	return ef.Apply()
+}
+
+func WithEnv(args []string) error {
+	in_flag := ""
+	for _, f := range args {
+		switch {
+		case f == "--env" || f == "-e":
+			in_flag = "env"
+		case in_flag == "env":
+			log.Info("Applying  env: ", f)
+			action := EnvFile{path: f}
+			action.Apply()
+			in_flag = ""
+
+		case f == "--script" || f == "-s":
+			in_flag = "script"
+		case in_flag == "script":
+			log.Info("Applying script: ", f)
+			action := EnvScript{cmd: f}
+			action.Apply()
+			in_flag = ""
+
+		default:
+			break
+		}
+	}
+
 	return nil
 }
