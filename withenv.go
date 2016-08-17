417fd008f272cddad1dcce6ef19b274fad34fadb
Eric Larson <eric@ionrock.org>
2016-08-17 10:45:23 -0500

    Working on allowing json and yaml

--- withenv.go
+++ withenv.go
@@ -1,13 +1,16 @@
 package bach
 
 import (
-	"gopkg.in/yaml.v2"
+	"bufio"
+	"bytes"
+	"encoding/json"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"strings"
 
 	log "github.com/Sirupsen/logrus"
+	"gopkg.in/yaml.v2"
 )
 
 func loadYaml(path string) ([]map[interface{}]interface{}, error) {
@@ -28,8 +31,25 @@ func loadYaml(path string) ([]map[interface{}]interface{}, error) {
 	return m, nil
 }
 
-func flatKey(prefix []string, key string) string {
-	return strings.Join(append(prefix, key), "_")
+func loadJson(path string) ([]map[interface{}]interface{}, error) {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	// try loading into a list of maps first
+	m := make(map[string]interface{}, 5)
+
+	err = json.Unmarshal(b, &m)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	log.Info(m)
+
+	out := make([]map[interface{}]interface{}, 1)
+	return out, nil
 }
 
 func flattenMap(env map[string]string, ev map[interface{}]interface{}, prefix []string) map[string]string {
@@ -58,19 +78,45 @@ func flattenEnv(env []map[interface{}]interface{}) map[string]string {
 	return fenv
 }
 
-func yamlToEnv(path string) (map[string]string, error) {
-	env, err := loadYaml(path)
+type envParser func(string) ([]map[interface{}]interface{}, error)
+
+func findParser(path string) envParser {
+	json := []string{"json"}
+	yaml := []string{"yaml", "yml"}
+
+	// Use the file extension first
+	for _, sfx := range json {
+		if strings.HasSuffix(path, sfx) {
+			return loadJson
+		}
+	}
+
+	for _, sfx := range yaml {
+		if strings.HasSuffix(path, sfx) {
+			return loadYaml
+		}
+	}
+
+	// peek at the first bytes for a {
+	f, err := os.Open(path)
 	if err != nil {
-		return nil, err
+		log.Fatal(err)
 	}
 
-	e := flattenEnv(env)
-	log.Debug("Flattened Env")
-	for k, v := range e {
-		log.Debugf("%s = %s", k, v)
+	reader := bufio.NewReader(f)
+	start, err := reader.Peek(3)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	return e, nil
+	f.Close()
+
+	// if the { exists, we'll try json
+	if bytes.HasPrefix(bytes.TrimLeft(start, " "), []byte("{")) {
+		return loadJson
+	}
+
+	return loadYaml
 }
 
 type Action interface {
@@ -81,8 +127,25 @@ type EnvFile struct {
 	path string
 }
 
+func (e EnvFile) Parse() (map[string]string, error) {
+	parser := findParser(e.path)
+
+	data, err := parser(e.path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	env := flattenEnv(data)
+	log.Debug("Flattened Env")
+	for k, v := range env {
+		log.Debugf("%s = %s", k, v)
+	}
+
+	return env, nil
+}
+
 func (e EnvFile) Apply() map[string]string {
-	env, err := yamlToEnv(e.path)
+	env, err := e.Parse()
 	if err != nil {
 		panic(err)
 	}
@@ -103,6 +166,7 @@ type EnvScript struct {
 }
 
 func (e EnvScript) Apply() map[string]string {
+	// TODO: Figure out a good way to delete this file
 	tmp, err := ioutil.TempFile("", "")
 	if err != nil {
 		panic(err)
