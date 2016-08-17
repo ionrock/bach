e3d1cd865d37c526a29a100a043f95c5df2ca607
Eric Larson <eric@ionrock.org>
2016-08-17 13:59:06 -0500

    Support a script outputing json or yaml

--- flatten.go
+++ flatten.go
@@ -1,38 +1,117 @@
 package bach
 
 import (
+	"bufio"
+	"bytes"
 	"encoding/json"
 	"io/ioutil"
+	"os"
 	"strings"
 
 	log "github.com/Sirupsen/logrus"
+	"github.com/ghodss/yaml"
 )
 
+func NewFlattener(path string) Flattener {
+	json := []string{"json"}
+	yaml := []string{"yaml", "yml"}
+
+	// Use the file extension first
+	for _, sfx := range json {
+		if strings.HasSuffix(path, sfx) {
+			log.Debug("Using JSON parser")
+			return JsonFlattener{path}
+		}
+	}
+
+	for _, sfx := range yaml {
+		if strings.HasSuffix(path, sfx) {
+			log.Debug("Using YAML parser")
+			return YamlFlattener{path}
+		}
+	}
+
+	// peek at the first bytes for a {
+	f, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reader := bufio.NewReader(f)
+	start, err := reader.Peek(3)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	f.Close()
+
+	// if the { exists, we'll try json
+	if bytes.HasPrefix(bytes.TrimLeft(start, " "), []byte("{")) {
+		log.Debug("Using JSON parser")
+		return JsonFlattener{path}
+	}
+
+	// if the [ exists, we'll try json
+	if bytes.HasPrefix(bytes.TrimLeft(start, " "), []byte("[")) {
+		log.Debug("Using JSON parser")
+		return JsonFlattener{path}
+	}
+
+	log.Debug("Using YAML parser")
+	return YamlFlattener{path}
+}
+
 type Flattener interface {
-	Flatten(string) map[string]string
+	Flatten() (map[string]string, error)
 }
 
 type JsonFlattener struct {
 	path string
 }
 
-func (f JsonFlattener) load(path string) (map[string]interface{}, error) {
-	b, err := ioutil.ReadFile(path)
-	if err != nil {
-		return nil, err
-	}
-
+func (f JsonFlattener) loadMap(b []byte) ([]map[string]interface{}, error) {
 	// try loading into a list of maps first
-	m := make(map[string]interface{}, 5)
+	m := make(map[string]interface{})
 
-	err = json.Unmarshal(b, &m)
+	err := json.Unmarshal(b, &m)
 	if err != nil {
 		log.Error(err)
 		return nil, err
 	}
 
-	log.Info(m)
+	return []map[string]interface{}{m}, nil
+}
 
+func (f JsonFlattener) loadList(b []byte) ([]map[string]interface{}, error) {
+	// try loading into a list of maps first
+	m := make([]map[string]interface{}, 5)
+
+	err := json.Unmarshal(b, &m)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	return m, nil
+}
+
+func (f JsonFlattener) load(path string) ([]map[string]interface{}, error) {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	m, err := f.loadMap(b)
+	if err == nil {
+		return m, err
+	}
+
+	log.Errorf("Error parsing %s as an object", path)
+
+	m, err = f.loadList(b)
+	if err != nil {
+		log.Fatal(err)
+	}
 	return m, nil
 }
 
@@ -43,7 +122,6 @@ func flatKey(prefix []string, key string) string {
 func (f JsonFlattener) flattenEnv(env []map[string]interface{}) map[string]string {
 	fenv := make(map[string]string)
 	for _, ev := range env {
-		// must be a map
 		f.flattenMap(fenv, ev, []string{})
 	}
 	return fenv
@@ -64,4 +142,72 @@ func (f JsonFlattener) flattenMap(env map[string]string, ev map[string]interface
 	}
 
 	return env
+}
+
+func (f JsonFlattener) Flatten() (map[string]string, error) {
+	env, err := f.load(f.path)
+	if err != nil {
+		log.Error("Error loading JSON")
+		return nil, err
+	}
+
+	return f.flattenEnv(env), nil
+}
+
+type YamlFlattener struct {
+	path string
+}
+
+func (f YamlFlattener) load(path string) ([]map[interface{}]interface{}, error) {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	// try loading into a list of maps first
+	m := make([]map[interface{}]interface{}, 5)
+
+	err = yaml.Unmarshal(b, &m)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	return m, nil
+}
+
+func (f YamlFlattener) flattenMap(env map[string]string, ev map[interface{}]interface{}, prefix []string) map[string]string {
+	for k, v := range ev {
+		switch t := v.(type) {
+		case string:
+			key := flatKey(prefix, k.(string))
+			env[key] = v.(string)
+		case map[interface{}]interface{}:
+			f.flattenMap(env, v.(map[interface{}]interface{}), append(prefix, k.(string)))
+		default:
+			log.Debug("Prefix: ", prefix)
+			log.Debugf("Default: %#v", t)
+		}
+	}
+
+	return env
+}
+
+func (f YamlFlattener) flattenEnv(env []map[interface{}]interface{}) map[string]string {
+	fenv := make(map[string]string)
+	for _, ev := range env {
+		// must be a map
+		f.flattenMap(fenv, ev, []string{})
+	}
+	return fenv
+}
+
+func (f YamlFlattener) Flatten() (map[string]string, error) {
+	env, err := f.load(f.path)
+	if err != nil {
+		log.Error("Error loading YAML")
+		return nil, err
+	}
+
+	return f.flattenEnv(env), nil
 }
--- withenv.go
+++ withenv.go
@@ -1,123 +1,12 @@
 package bach
 
 import (
-	"bufio"
-	"bytes"
-	"encoding/json"
 	"io/ioutil"
 	"os"
 	"os/exec"
-	"strings"
 
 	log "github.com/Sirupsen/logrus"
-	"gopkg.in/yaml.v2"
 )
-
-func loadYaml(path string) ([]map[interface{}]interface{}, error) {
-	b, err := ioutil.ReadFile(path)
-	if err != nil {
-		return nil, err
-	}
-
-	// try loading into a list of maps first
-	m := make([]map[interface{}]interface{}, 5)
-
-	err = yaml.Unmarshal(b, &m)
-	if err != nil {
-		log.Error(err)
-		return nil, err
-	}
-
-	return m, nil
-}
-
-func loadJson(path string) ([]map[interface{}]interface{}, error) {
-	b, err := ioutil.ReadFile(path)
-	if err != nil {
-		return nil, err
-	}
-
-	// try loading into a list of maps first
-	m := make(map[string]interface{}, 5)
-
-	err = json.Unmarshal(b, &m)
-	if err != nil {
-		log.Error(err)
-		return nil, err
-	}
-
-	log.Info(m)
-
-	out := make([]map[interface{}]interface{}, 1)
-	return out, nil
-}
-
-func flattenMap(env map[string]string, ev map[interface{}]interface{}, prefix []string) map[string]string {
-	for k, v := range ev {
-		switch t := v.(type) {
-		case string:
-			key := flatKey(prefix, k.(string))
-			env[key] = v.(string)
-		case map[interface{}]interface{}:
-			flattenMap(env, v.(map[interface{}]interface{}), append(prefix, k.(string)))
-		default:
-			log.Debug("Prefix: ", prefix)
-			log.Debugf("Default: %#v", t)
-		}
-	}
-
-	return env
-}
-
-func flattenEnv(env []map[interface{}]interface{}) map[string]string {
-	fenv := make(map[string]string)
-	for _, ev := range env {
-		// must be a map
-		flattenMap(fenv, ev, []string{})
-	}
-	return fenv
-}
-
-type envParser func(string) ([]map[interface{}]interface{}, error)
-
-func findParser(path string) envParser {
-	json := []string{"json"}
-	yaml := []string{"yaml", "yml"}
-
-	// Use the file extension first
-	for _, sfx := range json {
-		if strings.HasSuffix(path, sfx) {
-			return loadJson
-		}
-	}
-
-	for _, sfx := range yaml {
-		if strings.HasSuffix(path, sfx) {
-			return loadYaml
-		}
-	}
-
-	// peek at the first bytes for a {
-	f, err := os.Open(path)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	reader := bufio.NewReader(f)
-	start, err := reader.Peek(3)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	f.Close()
-
-	// if the { exists, we'll try json
-	if bytes.HasPrefix(bytes.TrimLeft(start, " "), []byte("{")) {
-		return loadJson
-	}
-
-	return loadYaml
-}
 
 type Action interface {
 	Apply() map[string]string
@@ -128,15 +17,13 @@ type EnvFile struct {
 }
 
 func (e EnvFile) Parse() (map[string]string, error) {
-	parser := findParser(e.path)
+	f := NewFlattener(e.path)
 
-	data, err := parser(e.path)
+	env, err := f.Flatten()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	env := flattenEnv(data)
-	log.Debug("Flattened Env")
 	for k, v := range env {
 		log.Debugf("%s = %s", k, v)
 	}
@@ -166,11 +53,11 @@ type EnvScript struct {
 }
 
 func (e EnvScript) Apply() map[string]string {
-	// TODO: Figure out a good way to delete this file
 	tmp, err := ioutil.TempFile("", "")
 	if err != nil {
 		panic(err)
 	}
+	defer os.Remove(tmp.Name())
 
 	parts := SplitCommand(e.cmd)
 
@@ -185,7 +72,6 @@ func (e EnvScript) Apply() map[string]string {
 	tmp.Close()
 
 	ef := EnvFile{path: tmp.Name()}
-
 	return ef.Apply()
 }
 
