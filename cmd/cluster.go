a7b31bca7b165ba065e62505d49e53304025b1d1
Eric Larson <eric@ionrock.org>
2016-08-24 11:56:20 +0100

    Move cluster to its own command and fix leaving the cluster
    This moves clustering to its own command as it is really a different
    topic than pre/post scripts. I'd also like the cluster bit to add to
    the environment itself rather than having to do something like:
    
      we -s 'cluster nodes 172.18.0.4:7546' cluster -n foo -c 172.18.0.4:7546 myapp
    
    Instead you'd just do:
    
     cluster -n foo -c 172.18.0.4:7546 toconfig myapp
    
    Also, I had used the wrong struct when leaving the cluster, which gave
    me a infinite loop!

---
+++ cmd/cluster.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"time"
+
+	"github.com/codegangsta/cli"
+	"github.com/ionrock/bach"
+)
+
+func ClusterAction(c *cli.Context) error {
+	name := c.Args()[0]
+	addr := ""
+	if len(c.Args()) > 1 {
+		addr = c.Args()[1]
+	}
+
+	config := bach.LocalConfig(name)
+	if c.Int("port") != 0 {
+		config.BindPort = c.Int("port")
+	}
+
+	sm := &bach.ServiceMap{
+		Config:      config,
+		ClusterAddr: addr,
+	}
+
+	sm.Load()
+	sm.Join()
+
+	log.Printf("Listing on: %s:%d", config.BindAddr, config.BindPort)
+	for {
+		time.Sleep(time.Second)
+	}
+	return nil
+}
+
+func ClusterNodesAction(c *cli.Context) error {
+	args := c.Args()
+
+	if len(args) == 0 {
+		log.Fatal("No cluster address provided")
+	}
+	addr := args[0]
+
+	config := bach.LocalConfig("tmp")
+
+	config.LogOutput = ioutil.Discard
+
+	sm := &bach.ServiceMap{
+		Config:      config,
+		ClusterAddr: addr,
+	}
+
+	sm.Join()
+	sm.CopyJsonTo(os.Stdout)
+
+	return nil
+}
+
+var sm *bach.ServiceMap
+
+func JoinClusterBefore(c *cli.Context) error {
+	if c.String("name") != "" && c.String("cluster-addr") != "" {
+		config := bach.LocalConfig(c.String("name"))
+		sm = &bach.ServiceMap{
+			Config:      config,
+			ClusterAddr: c.String("cluster-addr"),
+		}
+		sm.Join()
+	}
+
+	return nil
+}
+
+func LeaveClusterAfter(c *cli.Context) error {
+	fmt.Println("Leaving!!!")
+	err := sm.Leave()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
+}
+
+func GetHereApp() *cli.App {
+	app := cli.NewApp()
+
+	app.Commands = []cli.Command{
+		cli.Command{
+			Name:   "start",
+			Usage:  "Manually connect to / create a cluster",
+			Action: ClusterAction,
+			Flags: []cli.Flag{
+				cli.IntFlag{
+					Name: "port, p",
+				},
+			},
+		},
+		cli.Command{
+			Name:   "nodes",
+			Usage:  "Connect to a cluster and echo a JSON list of members",
+			Action: ClusterNodesAction,
+		},
+	}
+
+	app.Before = JoinClusterBefore
+	app.After = LeaveClusterAfter
+	app.Action = bach.CommandAction
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "name, n",
+			Value:  "",
+			EnvVar: "BACH_CLUSTER_NAME",
+			Usage:  "The name of an app to a cluster",
+		},
+		cli.StringFlag{
+			Name:   "cluster-addr, c",
+			Value:  "",
+			EnvVar: "BACH_CLUSTER_ADDR",
+			Usage:  "The address of another node in the cluster",
+		},
+	}
+	return app
+}
+
+func main() {
+	app := GetHereApp()
+	app.Run(os.Args)
+}
--- cmd/present.go
+++ cmd/present.go
@@ -1,75 +1,13 @@
 package main
 
 import (
-	"io/ioutil"
-	"log"
 	"os"
-	"time"
 
 	"github.com/codegangsta/cli"
 	"github.com/ionrock/bach"
 )
 
-func ClusterAction(c *cli.Context) error {
-	name := c.Args()[0]
-	addr := ""
-	if len(c.Args()) > 1 {
-		addr = c.Args()[1]
-	}
-
-	config := bach.LocalConfig(name)
-	if c.Int("port") != 0 {
-		config.BindPort = c.Int("port")
-	}
-
-	sm := &bach.ServiceMap{
-		Config:      config,
-		ClusterAddr: addr,
-	}
-
-	sm.Load()
-	sm.Join()
-
-	log.Printf("Listing on: %s:%d", config.BindAddr, config.BindPort)
-	for {
-		time.Sleep(time.Second)
-	}
-	return nil
-}
-
-func ClusterNodesAction(c *cli.Context) error {
-	args := c.Args()
-
-	if len(args) == 0 {
-		log.Fatal("No cluster address provided")
-	}
-	addr := args[0]
-
-	config := bach.LocalConfig("tmp")
-
-	config.LogOutput = ioutil.Discard
-
-	sm := &bach.ServiceMap{
-		Config:      config,
-		ClusterAddr: addr,
-	}
-
-	sm.Join()
-	sm.CopyJsonTo(os.Stdout)
-
-	return nil
-}
-
 func RunScriptBefore(c *cli.Context) error {
-	if c.String("name") != "" && c.String("cluster-addr") != "" {
-		config := bach.LocalConfig(c.String("name"))
-		sm := &bach.ServiceMap{
-			Config:      config,
-			ClusterAddr: c.String("cluster-addr"),
-		}
-		sm.Join()
-	}
-
 	return bach.RunScript(c.String("start"))
 }
 
@@ -79,24 +17,6 @@ func RunScriptAfter(c *cli.Context) error {
 
 func GetHereApp() *cli.App {
 	app := cli.NewApp()
-
-	app.Commands = []cli.Command{
-		cli.Command{
-			Name:   "cluster",
-			Usage:  "Manually connect to / create a cluster",
-			Action: ClusterAction,
-			Flags: []cli.Flag{
-				cli.IntFlag{
-					Name: "port, p",
-				},
-			},
-		},
-		cli.Command{
-			Name:   "nodes",
-			Usage:  "Connect to a cluster and echo a JSON list of members",
-			Action: ClusterNodesAction,
-		},
-	}
 
 	app.Before = RunScriptBefore
 	app.After = RunScriptAfter
@@ -113,16 +33,6 @@ func GetHereApp() *cli.App {
 			Value:  "",
 			EnvVar: "BACH_PRESENT_AFTER",
 			Usage:  "A script / cmd to run when finishing your process.",
-		},
-		cli.StringFlag{
-			Name:  "name, n",
-			Value: "",
-			Usage: "The name of an app to a cluster",
-		},
-		cli.StringFlag{
-			Name:  "cluster-addr, c",
-			Value: "",
-			Usage: "The address of another node in the cluster",
 		},
 	}
 	return app
