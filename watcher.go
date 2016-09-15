4ed2a1e668c76703d4758964d3bd365920f5eff4
Eric Larson <eric@ionrock.org>
2016-09-15 09:54:20 -0500

    Add initial watcher struct
    This isn't used just yet but will be used to run commands on a
    candence and restart processes when something changes.

---
+++ watcher.go
@@ -0,0 +1,24 @@
+package bach
+
+import (
+	"time"
+)
+
+type Watch struct {
+	Command string
+	Action  string
+	Cadence uint64
+}
+
+func (w *Watch) Start() {
+	ticker := time.NewTicker(time.Duration(w.Cadence))
+	cmd := SplitCommand(w.Command)
+	action := SplitCommand(w.Action)
+
+	for range ticker.C {
+		err := RunLogged(cmd...)
+		if err != nil {
+			RunLogged(action...)
+		}
+	}
+}
